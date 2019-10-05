50441b125424e2e628385a148e19701d836b96ed
Daniel Chen <dc165@berkeley.edu>
2019-10-04 21:18:26 -0700

    Creates warriors.csv

--- code/demo-script.R
+++ code/demo-script.R
@@ -9,3 +9,6 @@
 library(dplyr)    # data wrangling
 library(ggplot2)  # graphics
 
+data <- read.csv('../data/nba2018-players.csv')
+warriors <- arrange(filter(data, data$team == 'GSW'), salary)
+write.csv(warriors, '../data/warriors.csv', row.names = FALSE)
