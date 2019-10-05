fe845ef7ee03e3a5e9563d4aac3f46a9b36f9bed
Daniel Chen <dc165@berkeley.edu>
2019-10-04 21:03:11 -0700

    mostly empty file

---
+++ code/demo-script.R
@@ -0,0 +1,11 @@
+# Title: Demo script for Lab05
+# Description: what the script is about (one paragraph or two)
+# Input(s): what are the main inputs (list of inputs)
+# Output(s): what are the main outputs (list of outputs)
+# Author: Daniel Chen
+# Date: 10-04-2019
+
+# packages
+library(dplyr)    # data wrangling
+library(ggplot2)  # graphics
+
